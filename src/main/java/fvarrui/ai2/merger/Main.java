b2b1cc1546824e522564fc530935af4c318cecb6
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-11-29 13:53:46 +0000

    U

--- src/main/java/fvarrui/ai2/merger/Main.java
+++ src/main/java/fvarrui/ai2/merger/Main.java
@@ -1,9 +1,11 @@
 package fvarrui.ai2.merger;
 
+import edu.mills.feeney.thesis.aimerger.AIMerger;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		AIMerger.main(args);
 	}
 
 }
